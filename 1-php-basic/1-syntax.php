44c6efc43e9444d2ac4305f2793b1c45c4b1e6df
namnh06 <namnh.website@gmail.com>
2018-05-01 14:47:51 +0700

    add slides

---
+++ 1-php-basic/1-syntax.php
@@ -0,0 +1,46 @@
+
+<?php
+echo "<a href=\"./\">BACK</a>";
+/* 1. --- SYNTAX ---
+ *
+ * PHP code always starts with <?php and end with ?>
+ * ?> no need in php files
+ * Can be use php code in html with tag <?php and end with ?>
+ *
+ * NOTE :
+ * The end of the code of PHP always need semicolon ;
+ *
+ */
+echo "<h1>1. SYNTAX</h1>";
+/*
+ * EXERCISE 1 : Code HTML in this file outside the <?php and ?>, display your name and the project which you wanna do.
+ */
+
+/*
+ * SUGGESTION :
+ * <!DOCTYPE html>
+ * <html>
+ * <header>
+ * </header>
+ * <body>
+ * <h1>Hello World</h1>
+ * </body>
+ * </html>
+ */
+
+echo "<br><br>DO EXERCISE INSIDE COMMENT CODE BELOW THIS LINE<hr>";
+?>
+
+
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+echo "Hello World";
