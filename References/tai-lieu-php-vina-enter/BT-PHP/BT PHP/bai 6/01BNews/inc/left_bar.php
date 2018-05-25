be31faf6010d36f6311741dc79f7858340f3f370
namnh06 <namnh.website@gmail.com>
2018-05-25 22:04:21 +0700

    added slide html 0 and 1

--- References/tai-lieu-php-vina-enter/BT-PHP/BT PHP/bai 6/01BNews/inc/left_bar.php
+++
@@ -1,7 +0,0 @@
-<h2>Danh mục tin</h2>
-<ul>
-	<li><a href="cat.php">Thời sự</a></li>
-	<li><a href="cat.php">Góc nhìn</a></li>
-	<li><a href="cat.php">Thế giới</a></li>
-	<li><a href="cat.php">Kinh doanh</a></li>
-</ul>
